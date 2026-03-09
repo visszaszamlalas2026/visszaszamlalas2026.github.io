8eb280b4772395865e0397bf32bba7952a93aaad
visszaszamlalas2026 <visszaszamlalas2026@tutamail.com>
2026-03-09 22:31:53 +0100

    create consts.ts

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+import dayjs from "dayjs";
+
+export const DATE_FORMAT = "YYYY-MM-DD";
+export const DATE_SHORT_FORMAT = "MM. DD.";
+export const startDate = dayjs("2026-01-13", DATE_FORMAT);
+export const endDate = dayjs("2026-04-12", DATE_FORMAT);
