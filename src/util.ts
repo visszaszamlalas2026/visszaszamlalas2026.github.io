7a1d538628029b3630e94157a15972257a46bc36
visszaszamlalas2026 <visszaszamlalas2026@tutamail.com>
2026-03-09 23:01:12 +0100

    add util.ts

---
+++ src/util.ts
@@ -0,0 +1,7 @@
+export const splitArray = (array: any[], chunkSize: number) => {
+  let result = [];
+  for (let i = 0; i < array.length; i += chunkSize) {
+    result.push(array.slice(i, i + chunkSize));
+  }
+  return result;
+};
