59c765305be1158b777d6be266c4e397e79b54b5
visszaszamlalas2026 <visszaszamlalas2026@tutamail.com>
2026-03-09 23:40:55 +0100

    WIP more calendar day types

--- src/components/DayButton.tsx
+++ src/components/DayButton.tsx
@@ -5,29 +5,56 @@ const colors = ["#d83423", "#eee", "#32a823"];
 
 enum DayButtonType {
   Future,
-  TodayExists,
-  // TodayMissing,
-  PastExists,
-  // PastMissing
+  TodayExistsClosed,
+  TodayExistsOpened,
+  TodayMissing,
+  PastExistsClosed,
+  PastExistsOpened,
+  PastMissing
 };
 
-const dayToType = (day: Dayjs): DayButtonType => {
+const dayToType = (day: Dayjs, isOpened: boolean): DayButtonType => {
   if (day.isBefore(TODAY, 'day')) {
-    return DayButtonType.PastExists;
+    return isOpened ? DayButtonType.PastExistsOpened : DayButtonType.PastExistsClosed;
   } else if (day.isSame(TODAY, 'day')) {
-    return DayButtonType.TodayExists;
+    return isOpened ? DayButtonType.TodayExistsOpened : DayButtonType.TodayExistsClosed;
   } else {
     return DayButtonType.Future;
   }
 }
 
+const dayButtonTypeToString = (dayButtonType: DayButtonType) => {
+  switch (dayButtonType) {
+    case DayButtonType.Future: return "future";
+    case DayButtonType.TodayExistsClosed: return "today closed";
+    case DayButtonType.TodayExistsOpened: return "today opened";
+    case DayButtonType.TodayMissing: return "today missing lol";
+    case DayButtonType.PastExistsClosed: return "past closed";
+    case DayButtonType.PastExistsOpened: return "past opened";
+    case DayButtonType.PastMissing: return "past missing lol";
+  }
+}
+
+const dayButtonTypeToCursor = (dayButtonType: DayButtonType) => {
+  switch (dayButtonType) {
+    case DayButtonType.Future: return "wait";
+    case DayButtonType.TodayExistsClosed: return "pointer";
+    case DayButtonType.TodayExistsOpened: return "pointer";
+    case DayButtonType.TodayMissing: return "not-allowed";
+    case DayButtonType.PastExistsClosed: return "pointer";
+    case DayButtonType.PastExistsOpened: return "pointer";
+    case DayButtonType.PastMissing: return "not-allowed";
+  }
+}
+
 export const DayButton = (props: { day: Dayjs, index: number, isOpened: boolean, doOpen: Function }) => {
 
-  const thisType = dayToType(props.day);
+  const thisType = dayToType(props.day, props.isOpened);
 
-  return <div onClick={() => { props.doOpen(); }}>
+  const canBeOpened = thisType == DayButtonType.TodayExistsClosed || thisType == DayButtonType.PastExistsClosed;
+
+  return <div onClick={canBeOpened ? () => { props.doOpen(); } : () => { }} style={`cursor: ${dayButtonTypeToCursor(thisType)}`}>
     <span style={`background-color: ${colors[props.index % colors.length]}`}>{props.day.format(DATE_SHORT_FORMAT)}</span>
-    {thisType == DayButtonType.Future ? "future" : (thisType == DayButtonType.TodayExists ? "TODAY" : "past")}
-    {props.isOpened ? "OPEN" : "closed"}
+    {dayButtonTypeToString(thisType)}
   </div>
 }
