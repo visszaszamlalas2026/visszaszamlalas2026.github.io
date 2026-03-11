ead0b86fbe2ac90cdb06c68c58f4c43f7e96263a
visszaszamlalas2026 <visszaszamlalas2026@tutamail.com>
2026-03-11 19:37:20 +0100

    much simpler google drive download, holy shit

--- gdrive/download.ts
+++ gdrive/download.ts
@@ -1,4 +1,3 @@
-import https from "node:https";
 import fs from "node:fs";
 
 const lines = fs.readFileSync('./gdown_logs.txt', 'utf-8')
@@ -14,28 +13,9 @@ for (let line of lines) {
   const downloadUrl = isImage ? `https://drive.usercontent.google.com/uc?id=${id}&export=download` : `https://docs.google.com/feeds/download/documents/export/Export?id=${id}&exportFormat=txt`;
   const targetFile = isImage ? `./${date}.jpg` : './' + line.split(' ').slice(3).join(' ') + '.txt';
 
-  console.log({ id, isImage, date, targetFile, downloadUrl });
+  await fetch(downloadUrl, { redirect: 'follow' }).then(async (resp) => {
+    await fs.writeFileSync(targetFile, Buffer.from(await resp.arrayBuffer()));
+  });
 
-  const downloadFile = async (url: string, target: string, cookie: string = '') => {
-    await new Promise(async (resolve, reject) => {
-      https.get(url, {
-        headers: {
-          Cookie: cookie
-        }
-      }, async (res) => {
-
-        console.log({ get: url, h: res.headers });
-
-        switch (res.statusCode) {
-          case 307: await downloadFile(url, res.headers.location as string, res.headers["set-cookie"] && res.headers["set-cookie"].length > 0 ? res.headers["set-cookie"].at(0) : '').then(resolve).catch(reject); break;
-          case 200: res.pipe(fs.createWriteStream(target))
-            .on('finish', resolve)
-            .on('error', reject); break;
-          default: throw new Error(res.statusCode?.toString());
-        }
-      }).on('error', reject);
-    });
-  };
-
-  await downloadFile(downloadUrl, targetFile);
+  console.log({ targetFile });
 }
