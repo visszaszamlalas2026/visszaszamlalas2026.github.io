b706c92fb68776b93382589f45771e5d9baf75ee
visszaszamlalas2026 <visszaszamlalas2026@tutamail.com>
2026-03-18 14:12:41 +0100

    one more fix around timezones maybe?

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,10 @@ import timezone from "dayjs/plugin/timezone";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
-dayjs.tz.setDefault('Europe/Budapest');
+
+export const TIMEZONE = 'Europe/Budapest';
+
+dayjs.tz.setDefault(TIMEZONE);
 
 export const CONTENT_COLLECTION = 'days';
 
@@ -17,6 +20,6 @@ export const DATE_REGEX = '\\d{4}-\\d{2}-\\d{2}';
 
 export const DATE_FORMAT = "YYYY-MM-DD";
 export const DATE_SHORT_FORMAT = "M. DD.";
-export const CALENDAR_START = dayjs("2026-01-12", DATE_FORMAT);
-export const CALENDAR_END = dayjs("2026-04-12", DATE_FORMAT);
-export const TODAY = dayjs();
+export const CALENDAR_START = dayjs.tz("2026-01-12 12:00:00", TIMEZONE);
+export const CALENDAR_END = dayjs.tz("2026-04-12 12:00:00", TIMEZONE);
+export const TODAY = dayjs().tz(TIMEZONE);
